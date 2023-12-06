6b7fbb807491ec5f79f101567e0cb73f1bfb42aa
Jon Emrich <jonathan.emrich@tixr.com>
2023-12-05 20:47:16 -0800

    updating run script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ MUSIC_FOLDER=./music/*
 for f in $(find $MUSIC_FOLDER -name '*.mp3'); do 
     if ! [ -f "${f%.mp3}.wav" ]; then
         echo "File does not exist. creating it"
-        ffmpeg -i "$f" -acodec pcm_s16le -ac 1 -ar 48000 "${f%.mp3}.wav"
+        ffmpeg -i "$f" -ar 48000 "${f%.mp3}.wav"
     fi
 done
 
