27a4fb37c0e32862f3207672c075aa2e378b99f1
Jon Emrich <126989196+jon-tixr@users.noreply.github.com>
2024-05-19 21:38:44 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ fi
 echo "Source folder: $SOURCE_FOLDER"
 echo "Destination folder: $MUSIC_FOLDER"
 
-find "$SOURCE_FOLDER" -name '*.mp3' -print0 | while IFS= read f; do
+find "$SOURCE_FOLDER" -name '*.mp3' -print0 | while IFS= read -d '' f; do
     # Determine the relative path of the .mp3 file
     relative_path="${f#"$SOURCE_FOLDER/"}"
     
