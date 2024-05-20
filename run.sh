b668965986a630a5cc0eda1ead34de06bbd03638
Jon Emrich <126989196+jon-tixr@users.noreply.github.com>
2024-05-19 20:58:25 -0700

    adding logging

--- run.sh
+++ run.sh
@@ -15,6 +15,9 @@ if [ -d "/media/BIRDS/music" ] && [ -f "/media/BIRDS/config_multi_song.json" ]; 
     SOURCE_FOLDER="/media/BIRDS/music"
 fi
 
+echo "Source folder: $SOURCE_FOLDER"
+echo "Destination folder: $MUSIC_FOLDER"
+
 find "$SOURCE_FOLDER" -name '*.mp3' -print0 | while IFS= read -r -d '' f; do
     # Determine the relative path of the .mp3 file
     relative_path="${f#"$SOURCE_FOLDER/"}"
