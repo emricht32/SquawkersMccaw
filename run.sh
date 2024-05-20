660083135c15c45da05cbea62dc8123614ddf71b
Jon Emrich <126989196+jon-tixr@users.noreply.github.com>
2024-05-19 23:00:48 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -34,33 +34,18 @@ find "$SOURCE_FOLDER" -name '*.mp3' -print0 | while IFS= read -r -d '' f; do
     echo "Destination directory: $dest_dir"
     echo "WAV file path: $wav_file"
 
-    if ! [ -f "$wav_file" ]; then
-        echo "Creating WAV file for $f"
-        if ! ffmpeg -i "$f" -ar 48000 "$wav_file"; then
-            echo "Error converting $f to WAV" >&2
-            exit 1
-        fi
-    else
-        echo "WAV file already exists for $f"
-    fi
+    # if ! [ -f "$wav_file" ]; then
+    #     echo "Creating WAV file for $f"
+    #     if ! ffmpeg -i "$f" -ar 48000 "$wav_file"; then
+    #         echo "Error converting $f to WAV" >&2
+    #         exit 1
+    #     fi
+    # else
+    #     echo "WAV file already exists for $f"
+    # fi
+
+    echo "*Processing file: $f"
+    echo "*Relative path: $relative_path"
+    echo "*Destination directory: $dest_dir"
+    echo "*WAV file path: $wav_file"
 done
-
-
-# Allow some time for processing if necessary
-sleep 5
-
-# Install Python dependencies
-if ! pip3 install -r pi-requirements.txt; then
-    echo "Error installing Python dependencies" >&2
-    exit 1
-fi
-
-# Pull the latest code from the repository
-# Uncomment if needed
-# git pull
-
-# Run the Python script
-if ! python3 src/main.py; then
-    echo "Error running the Python script" >&2
-    exit 1
-fi
