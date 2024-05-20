710b5a8636a09752386ccce77edf696daf5fc8c3
Jon Emrich <126989196+jon-tixr@users.noreply.github.com>
2024-05-19 21:39:54 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -34,15 +34,15 @@ find "$SOURCE_FOLDER" -name '*.mp3' -print0 | while IFS= read -d '' f; do
     echo "Destination directory: $dest_dir"
     echo "WAV file path: $wav_file"
 
-    if ! [ -f "$wav_file" ]; then
-        echo "Creating WAV file for $f"
-        if ! ffmpeg -i "$f" -ar 48000 "$wav_file"; then
-            echo "Error converting $f to WAV" >&2
-            exit 1
-        fi
-    else
-        echo "WAV file already exists for $f"
-    fi
+    # if ! [ -f "$wav_file" ]; then
+    #     echo "Creating WAV file for $f"
+    #     if ! ffmpeg -i "$f" -ar 48000 "$wav_file"; then
+    #         echo "Error converting $f to WAV" >&2
+    #         exit 1
+    #     fi
+    # else
+    #     echo "WAV file already exists for $f"
+    # fi
 done
 
 # Allow some time for processing if necessary
