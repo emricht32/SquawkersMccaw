d888bf68735ec9dd2d568a06b3f369f303f89106
Jon Emrich <emricht32@gmail.com>
2025-07-27 12:59:00 -0700

    birds dict

--- src/birdpi_main/main.py
+++ src/birdpi_main/main.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
         if current_index is not None:
             song = songs[current_index]
             song_name = song.get("name")
-            for b in registry.get_birds():
+            for b in registry.get_birds().values():
                 print("cancel_current_song.birds[].bird=",b)
                 post_cancel_to_bird(b, song_name)
         bird.cancel_current_song()
