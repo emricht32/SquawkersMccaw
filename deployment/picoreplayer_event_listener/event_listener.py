eb9df997a2a59e220fcf9071217314427d1745d8
Jon Emrich <jon.emrich@walmart.com>
2025-11-16 10:40:11 -0800

    adding logs

--- deployment/picoreplayer_event_listener/event_listener.py
+++ deployment/picoreplayer_event_listener/event_listener.py
@@ -72,9 +72,9 @@ def parseName(arg) -> str:
     arg1 = arg
     try:
         if isinstance(arg, list) and len(arg) > 0:
-            arg1 = arg[0]
-        log(f"parseName.d={str(arg1)}")
-        playlist = arg1.get("playlist_loop", [])
+            playlist = arg[0]["playlist_loop"]
+        # log(f"parseName.d={str(arg1)}")
+        # playlist = arg1.get("playlist_loop", [])
         log(f"playlist={str(playlist)}")
         if playlist and isinstance(playlist, list):
             first = playlist[0]
